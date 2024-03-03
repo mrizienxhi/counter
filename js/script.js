d6eb4d968bbb05c1a7e227916631a49e7147397b
Enxhi <mrizienxhi@yahoo.it>
2024-03-03 22:45:46 +0100

    id

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ const counterLabel = document.getElementById("label");
 
 let currentValue = 0;
 
-btnIncrement.addEventListener("click",() =>{
-    currentValue++;
-    counterLabel.textContent = currentValue;
+btnIncrement.addEventListener("click", () => {
+  currentValue++;
+  counterLabel.textContent = currentValue;
 });
 
-btnDecrement.addEventListener("click",() =>{
-    currentValue--;
-    counterLabel.textContent = currentValue;
+btnDecrement.addEventListener("click", () => {
+  currentValue--;
+  counterLabel.textContent = currentValue;
 });
 
-btnReset.addEventListener("click",() =>{
-    currentValue = 0;
-    counterLabel.textContent = currentValue;
+btnReset.addEventListener("click", () => {
+  currentValue = 0;
+  counterLabel.textContent = currentValue;
 });
